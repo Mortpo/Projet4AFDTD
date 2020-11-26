7543b5ffff2c1ad8a83440ef37efc6dfb5678f00
ANQUETIN Romain <50448246+Mortpo@users.noreply.github.com>
2020-11-26 13:48:40 +0100

    Add files via upload

---
+++ FDTD_sullivan.c
@@ -0,0 +1,359 @@
+// 3D simulation of a patch antenna.
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define IE 62
+#define JE 120
+#define KE 14
+#define ia 14
+#define ja 15
+#define ka 7
+#define NFREQS 3
+#define ktop 2
+
+void main()
+{
+    //initialisation , double>float ?
+    float dx[IE][JE][KE], dy[IE][JE][KE], dz[IE][JE][KE];
+    float ex[IE][JE][KE], ey[IE][JE][KE], ez[IE][JE][KE];
+    float hx[IE][JE][KE], hy[IE][JE][KE], hz[IE][JE][KE];
+    float gax[IE][JE][KE], gay[IE][JE][KE], gaz[IE][JE][KE];
+    int l, m, n, i, j, k, ic, jc, kc, nsteps, n_pml; //itérateurs ?
+    float ddz, ra_x, ra_y, dt, T, epsz, muz, pi, eaf, npml;
+    int ib, jb, kb; // coord cellule
+    float xn, xxn, xnim, xd;
+    float t0, spread, pulse;
+    FILE *fp, *fopen(), *fpt; //ouverture de fichier
+    float ez_inc[JE], hx_inc[JE];
+    float ez_low_m1, ez_low_m2, ez_high_m1, ez_high_m2;
+
+    //valeur en 3D de ?
+    float idxl[ia][JE][KE], idxh[ia][JE][KE];
+    float ihxl[ia][JE][KE], ihxh[ia][JE][KE];
+    float idyl[IE][ja][KE], idyh[IE][ja][KE];
+    float ihyl[IE][ja][KE], ihyh[IE][ja][KE];
+    float idzl[IE][JE][ka], idzh[IE][JE][ka];
+    float ihzl[IE][JE][ka], ihzh[IE][JE][ka];
+    int ixh, jyh, kzh;
+
+    //g et f lier a chaque cellule
+    float gi1[IE], gi2[IE], gi3[IE];
+    float gj1[JE], gj2[JE], gj3[JE];
+    float gk1[KE], gk2[KE], gk3[KE];
+    float fi1[IE], fi2[IE], fi3[IE];
+    float fj1[JE], fj2[JE], fj3[JE];
+    float fk1[KE], fk2[KE], fk3[KE];
+
+    float curl_h, curl_e;
+
+    int ii, jj, kk, numsph;
+    float dist, xdist, ydist, zdist;
+    int istart, iend, k_ref, jebd, i_ref;
+    int j_patch_st, j_patch_end, j_ref;
+    int i_patch_st, i_patch_end;
+    float eps_sub;
+    float shape[IE][KE], half_wv;
+
+    //p 123
+
+    ic = IE / 2;
+    jc = JE / 2;
+    kc = KE / 2;
+    ib = IE - ia - 1;
+    jb = JE - ja - 1;
+    kb = KE - ka - 1;
+
+    //definition de distances
+    i_patch_st = ia + 5;
+    i_patch_end = i_patch_st + 31;
+    j_patch_end = jb - 5;
+    j_patch_st = j_patch_end - 39;
+    j_ref = j_patch_st - 30;
+    k_ref = ktop - 1;
+
+    pi = 3.14159;
+    epsz = 8.8e-12;
+    muz = 4 * pi * 1.e-7;
+    ddz = 0.265e-3; //taille des cellules
+    ra_y = 0.6625;
+    ra_x = 0.6812;
+    dt = ddz / 6e8; //pas temporel
+
+    printf("%12.5e %12.5e    \n", ddz, dt);
+
+    // INITIALIZATION DES PARAMETRES, CALCUL DES PARAMETRES PML
+
+    //  Constante Dielectric du substrat
+
+    eps_sub = 2.2;
+    printf("%f  \n", eps_sub);
+
+    for (j = 0; ji < JE; j++)
+    {
+        for (i = 0; i < IE; i++)
+        {
+            for (k = 0; k < ktop; k++)
+            {
+                gax[i][j][k] = 1. / eps_sub;
+                gay[i][j][k] = 1. / eps_sub;
+                gax[i][j][k] = 1. / eps_sub;
+            }
+        }
+    }
+
+    //ajout de la plaque de métal à k=0
+
+    for (i = 1; i < IE - 1; i++)
+    {
+        for (j = 1; j < JE - 1; j++)
+        {
+            k = 0;
+            gax[i][j][k] = 0;
+            gaz[i][j][k] = 0;
+        }
+    }
+
+    istart = ia + 6;
+    iend = istart + 6;
+    i_ref = istart + (iend - istart) / 2;
+    printf("%d %d %d\n", istart, iend, i_ref);
+
+    half_wv = (iend - istart) / 2.;
+    printf("half_wv = %5.2f\n", half_wv);
+
+    for (i = istart; i <= iend; i++)
+    {
+        for (k = 0; k <= ktop; k++)
+        {
+            shape[i][k] = 1.;
+        }
+    }
+
+    // ajout du conducteur jusqu'au partch à k = ktop+1
+
+    for (j = 1; j <= j_patch_st; j++)
+    {
+        for (i = istart; i <= iend - 1; i++)
+        {
+            gax[i][j][ktop + 1] = 0.;
+        }
+        for (i = istart; i <= iend - 1; i++)
+        {
+            gay[i][j][ktop + 1] = 0.;
+        }
+    }
+
+    // ajout du patch à k = ktop
+
+    for (j = j_patch_st; j <= j_patch_end; j++)
+    {
+        for (i = ia + 1; i <= ib - 1; i++)
+        {
+            gax[i][j][ktop + 1] = 0.;
+        }
+        for (i = ia + 1; i <= ib - 1; i++)
+        {
+            gay[i][j][ktop + 1] = 0.;
+        }
+    }
+
+    t0 = 150.0;
+    spread = 25.0;
+    printf("pulse width is %12.5e  \n" spread * dt);
+    T = 0;
+    nsteps = 1;
+
+    fpt = fopen("Time", "w");
+
+    while (nsteps > o)
+    {
+        printf("nsteps --> ");
+        scanf("%d", &nsteps);
+        printf("%d  \n", nsteps);
+
+        for (n = 1; n < nsteps; n++)
+        {
+            T = T + 1;
+
+            ///////////////////////}? //surement pas car sinon on incrémente une variable
+            // Début de la boucle principale
+
+            //calcul du buffer incident
+            for (j = 1; j < JE; j++)
+            {
+                ez_inc[j] = gj3[j] * ez_inc[j] + gj2[j] * (.5 * ra_y / eps_sub) * (hx_inc[j - 1] - hx_inc[j]);
+            }
+
+            //Source
+            pulse = exp(-.5 * (pow((t0 - T) / spread, 2.0)));
+            ez_inc[ja - 2] = pulse;
+            printf("%4.0f %6.2f \n", T, pulse);
+
+            //calcul du champ Dx
+
+            for (i = 1; i < ia; i++)
+            {
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (ra_y * (hz[i][j][k] - hz[i][j - 1][k]) - hy[i][j][k] + hy[i][j][k - 1]);
+                        idxl[i][j][k] = idxl[i][j][k] + curl_h;
+                        dx[i][j][k] = gj3[j] * gk3[k] * dx[i][j][k] + gj2[j] * gk2[k] * .5 * (curl_h + gil[i] * idxl[i][j][k]);
+                    }
+                }
+            }
+
+            for (i = ia; i <= ib; i++)
+            {
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (ra_y * (hz[i][j][k] - hz[i][j - 1][k]) - hy[i][j][k] + hy[i][j][k - 1]);
+                        dx[i][j][k] = gj3[j] * gk3[k] * dx[i][j][k] + gj2[j] * gk2[k] * .5 * curl_h;
+                    }
+                }
+            }
+
+            for (i = ib + 1; i < IE; i++)
+            {
+                ixh = i - ib - 1;
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (ra_y * (hz[i][j][k] - hz[i][j - 1][k]) - hy[i][j][k] + hy[i][j][k - 1]);
+                        idxh[ixh][j][k] = idxh[ixh][j][k] + curl_h;
+                        dx[i][j][k] = gj3[j] * gk3[k] * dx[i][j][k] + gj2[j] * gk2[k] * .5 * (curl_h + gi1[i] * idxh[ixh][j][k]);
+                    }
+                }
+            }
+
+            //Calcul du champ Dy
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = 1; j < ja; j++)
+                {
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (hx[i][j][k] - hx[i][j][k - 1] - ra_x * (hz[i][j][k] - hz[i - 1][j][k]));
+                        idyl[i][j][k] = idyl[i][j][k] + curl_h;
+                        dy[i][j][k] = gi3[i] * gk3[k] * dy[i][j][k] + gj2[i] * gk2[k] * .5 * (curl_h + gjl[j] * idyl[i][j][k]);
+                    }
+                }
+            }
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = ja; j <= jb; j++)
+                {
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (hx[i][j][k] - hx[i][j][k - 1] - ra_x * (hz[i][j][k] - hz[i - 1][j][k]));
+                        idyl[i][j][k] = idyl[i][j][k] + curl_h;
+                        dy[i][j][k] = gi3[i] * gk3[k] * dy[i][j][k] + gj2[i] * gk2[k] * .5 * (curl_h + gjl[j] * idyl[i][j][k]);
+                    }
+                }
+            }
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = jb + 1; j <= JE; j++)
+                {
+                    jyh = j - jb - 1;
+                    for (k = 1; k < KE; k++)
+                    {
+                        curl_h = (hx[i][j][k] - hx[i][j][k - 1] - ra_x * (hz[i][j][k] - hz[i - 1][j][k]));
+                        idyl[i][jyh][k] = idyl[i][jyh][k] + curl_h;
+                        dy[i][j][k] = gi3[i] * gk3[k] * dy[i][j][k] + gj2[i] * gk2[k] * .5 * (curl_h + gjl[j] * idyh[i][jyh][k]);
+                    }
+                }
+            }
+
+            //calcul du champ DZ
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = 0; k < ka; k++)
+                    {
+                        curl_h = (ra_x * (hy[i][j][k] - hy[i - 1][j][k]) - ra_y * (hx[i][j][k] - hx[i][j - 1][k]));
+                        idzl[i][j][k] = idzl[i][j][k] + curl_h;
+                        dz[i][j][k] = gi3[i] * gj3[j] * dz[i][j][k] + gi2[i] * gj2[j] * .5 * (curl_h + gkl[k] * idzl[i][j][k]);
+                    }
+                }
+            }
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = ka; k <= kb; k++)
+                    {
+                        curl_h = (ra_x * (hy[i][j][k] - hy[i - 1][j][k]) - ra_y * (hx[i][j][k] - hx[i][j - 1][k]));
+                        dz[i][j][k] = gi3[i] * gj3[j] * dz[i][j][k] + gi2[i] * gj2[j] * .5 * curl_h;
+                    }
+                }
+            }
+
+            for (i = 1; i < IE; i++)
+            {
+                for (j = 1; j < JE; j++)
+                {
+                    for (k = kb + 1; k < KE; k++)
+                    {
+                        kzh = k - kb - 1;
+                        curl_h = (ra_x * (hy[i][j][k] - hy[i - 1][j][k]) - ra_y * (hx[i][j][k] - hx[i][j - 1][k]));
+                        idzh[i][j][kzh] = idzh[i][j][kzh] + curl_h;
+                        dz[i][j][k] = gi3[i] * gj3[j] * dz[i][j][k] + gi2[i] * gj2[j] * .5 * (curl_h + gkl[k] * idzl[i][j][kzh]);
+                    }
+                }
+            }
+
+            //Incident Dz
+
+            for (i = istart; i <= iend; i++)
+            {
+                for (k = 0; k <= ktop; k++)
+                {
+                    dz[i][ja][k] = dz[i][ja][k] + (.5 / eps_sub) * shape[i][k] * hx_inc[ja - 1];
+                }
+            }
+
+            // Calcul de E depuis le champ D
+
+            // Ex et Ey sont à 0 pour k=0
+
+            for (i = 1; i < IE - 1; i++)
+            {
+                for (j = 1; j < JE - 1; j++)
+                {
+                    for (k = 1; k < KE - 1; k++)
+                    {
+                        ex[i][j][k] = gax[i][j][k] * dx[i][j][k];
+                        ey[i][j][k] = gay[i][j][k] * dy[i][j][k];
+                        ez[i][j][k] = gaz[i][j][k] * dz[i][j][k];
+                    }
+                }
+            }
+
+            //Ecriture des données temporelles dans le port d'entré
+
+            fprintf(fpt, "%8.4f \n", ez[i_ref][j_ref][k_ref]);
+
+            //Calcul du champ incident
+
+            for (j = 0; j < JE - 1; j++)
+            {
+                hx_inc[j] = fj3[j] * hx_inc[j] + .5 * fj2[j] * (ez_inc[j] - ez_inc[j + 1]);
+            }
+
+            //Calcul de champ Hx (fin page 139)
+        }
+    }
+}
