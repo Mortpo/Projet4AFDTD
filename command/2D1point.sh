7309e5dc3be727fa2ad2b614e5544b5e4646541f
Mortpo <anquetin.r@gmail.com>
2020-12-23 18:08:01 +0100

    ajout de fonctionalité

---
+++ command/2D1point.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gcc -Wall -o ../release/main2D ../src/FDTD2D_sullivan.c -lm
+./../release/main2D
---
+++ command/3Dplane.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gcc -Wall -o ../release/main3D ../src/FDTD_sullivan.c -lm
+./../release/main3D
