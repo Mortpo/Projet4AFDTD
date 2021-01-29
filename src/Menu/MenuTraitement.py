e223dcc1a0368a178d6b1bb9ba479752193ba62b
Mortpo <anquetin.r@gmail.com>
2021-01-29 23:58:18 +0100

    mayavi gogo

--- src/Menu/MenuTraitement.py
+++ src/Menu/MenuTraitement.py
@@ -37,43 +37,49 @@ def getImageForAnimation(indice ,tailleX,tailleY, chemin):
     img = np.zeros((tailleX,tailleY))
     with open(chemin, newline='') as csvdata:
         datareader = csv.reader(csvdata, delimiter=' ')
-        for u in np.arange(indice*tailleY):
+        for u in range(indice*tailleY):
             datareader.__next__()
 
-        for u in np.arange(tailleX):
+        for u in range(tailleX):
             ligne = datareader.__next__()
             for i in np.arange(tailleY):
                 img[u][i]=float(ligne[i])
     return img
 
 
+
+
 def generate3DAnimation(start,end,Valmin,Valmax,tailleX,tailleY, chemin):
     
-    X, Y = np.meshgrid(tailleX,tailleY) 
-    fig = mlab.figure(size=(1920,1080))
-    img = np.zeros((60,60),dtype="float")
-    s = mlab.surf(img, warp_scale=25,vmin = Valmin, vmax = Valmax,colormap='viridis', figure = fig)
+    X, Y = np.meshgrid(tailleX,tailleY)
+    scale = 25
+    fig = mlab.figure(size=(480,360),bgcolor = (0,0,0))
+    img = np.zeros((tailleX,tailleY),dtype="float")
+    s = mlab.surf(img, warp_scale=scale,vmin = Valmin, vmax = Valmax,colormap='viridis', figure = fig)
     f = mlab.gcf()
     f.scene._lift()
+    mlab.view(azimuth= 180+45)
+    mlab.axes(figure = fig,nb_labels = 3,ranges = [0,tailleX, 0,tailleY,Valmin*scale ,Valmax*scale],xlabel="Delta X",ylabel="Delta Y",zlabel="Ez",x_axis_visibility=True,y_axis_visibility=False,z_axis_visibility=True)
+    mlab.colorbar(object=s, title="Champ Ez", orientation='vertical', nb_labels=None, nb_colors=255)
+    mlab.move(forward= -30)
+    files = []
     @mlab.animate(delay=20)
     def anim():
         for i in range(start,end):
             s.scene.disable_render = True
             img = getImageForAnimation(i,tailleX,tailleY, chemin)
             s.mlab_source.scalars=img
-            mlab.savefig("D:/ESIREM/4A/Python-microonde/Projet4AFDTD/render/img"+str(i)+".png")
+            stringPath = "D:/ESIREM/4A/Python-microonde/Projet4AFDTD/render/img"+str(i)+".png"
+            files.append( stringPath)
+            mlab.savefig(stringPath)
             s.scene.disable_render = False
             yield
 
     anim()
     mlab.show()
-    imdir = 'D:/ESIREM/4A/Python-microonde/Projet4AFDTD/render/'
-    ext = ['png']  
 
-    files = []
-    [files.extend(glob.glob(imdir + '*.' + e)) for e in ext]
     clip = mpy.ImageSequenceClip(sequence=files, fps=24)
-    clip.write_gif('2DSinusSansAxes.gif')
+    clip.write_gif('2DSinusSansAxesTest.gif')
     for filePath in files:
         try:
             os.remove(filePath)
@@ -99,7 +105,7 @@ def generate3DAnimation(start,end,Valmin,Valmax,tailleX,tailleY, chemin):
     plt.show()
 '''
 
-chemin = "D:/ESIREM/4A/Python-microonde/Projet4AFDTD/Data2D/Ez2D1pointpySin"
+chemin = "D:/ESIREM/4A/Python-microonde/Projet4AFDTD/Data2D/Ez2DpySin.data"
 tailleX , tailleY = 60,60
 #image = getImageForAnimation(0,tailleX , tailleY,chemin)
 #generateFixed3DSurface(image)
